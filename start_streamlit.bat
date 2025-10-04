21356b70e3b012a2a44bea301ad0506d24f1f3d0
Anupam Arya <anupamaryajsr@gmail.com>
2025-10-04 18:27:44 +0530

    Cleaned up project - ready for deployment

--- start_streamlit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Streamlit Mental Health Tweet Classifier Demo...
-cd /d "d:\Project\mental_health_tweet"
-echo. | streamlit run simple_demo.py --server.port 8501 --server.address localhost
-pause
